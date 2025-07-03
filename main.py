ed5c44dbe9e7fd05d39e7ea80c3a7dced8c626a6
Kani1986 <rpgibson1986@gmail.com>
2025-07-03 20:20:28 +0100

    slight change to how we check if argment was passed

--- main.py
+++ main.py
@@ -11,8 +11,7 @@ def main():
     if  len(sys.argv) < 2:
         print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
-    else:
-        file_path = sys.argv[1]
+    file_path = sys.argv[1]
     book_text = get_book_text(file_path)
     num_words = count_words(book_text)
     print(f"Found {num_words} total words")
